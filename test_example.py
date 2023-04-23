aea9b4848a4c926f0a29f1c338d111ea504cf7ad
RobertBykowski <trebor.media@gmail.com>
2023-04-23 17:09:22 +0200

    Update test_example.py

--- test_example.py
+++ test_example.py
@@ -3,17 +3,5 @@ def test_dodaj():
     assert dodaj(2, 3) == 5
     
 
-def test_odejmij():
-    assert odejmij(5, 3) == 2
-    
 
-def test_mnoz():
-    assert mnoz(2, 3) == 6
-   
-
-def test_dziel():
-    assert dziel(6, 3) == 2
-   
-    with pytest.raises(ValueError):
-        dziel(6, 0)
 
