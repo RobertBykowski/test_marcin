f78ee3e22d583d0949cebbfaf8255372b00dce30
RobertBykowski <trebor.media@gmail.com>
2023-04-23 17:17:31 +0200

    Update test_example.py

--- test_example.py
+++ test_example.py
@@ -1,7 +1,20 @@
-import pytest 
-from kalkulator import dodaj
+from kalkulator import dodaj, odejmij, pomnoz, podziel
+import pytest
+
 def test_dodaj():
     assert dodaj(2, 3) == 5
+
+def test_odejmij():
+    assert odejmij(5, 2) == 3
+
+def test_pomnoz():
+    assert pomnoz(4, 6) == 24
+
+def test_podziel():
+    assert podziel(10, 2) == 5
+    with pytest.raises(ZeroDivisionError):
+        podziel(5, 0)
+
     
 
 
